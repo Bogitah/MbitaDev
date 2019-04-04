f34e03f43759d4653b7b2e061ff2eff4be3201ba
Bogitah <concybogita@gmail.com>
2019-04-04 13:24:50 +0300

    manifest layout

--- app/src/main/java/com/example/mbitaferrydev/ManifestActivity.java
+++ app/src/main/java/com/example/mbitaferrydev/ManifestActivity.java
@@ -1,20 +1,49 @@
 package com.example.mbitaferrydev;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ManifestActivity extends AppCompatActivity {
+    Button btnsetdate;
+    DatePickerDialog datePickerDialog;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manifest);
 
+        btnsetdate = findViewById(R.id.btnmanifest);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        btnsetdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(ManifestActivity.this,
+                        (datePicker, year1, month1, day) -> btnsetdate.setText(day + "/" + month1 + "/" + year1), year, month, dayOfMonth);
+                datePickerDialog.show();
+            }
+
+        });
+
     }
     @Override
     public boolean  onOptionsItemSelected(MenuItem item){
@@ -34,4 +63,6 @@ public class ManifestActivity extends AppCompatActivity {
         startActivity(backMainTest);
         finish();
     }
+
 }
+
